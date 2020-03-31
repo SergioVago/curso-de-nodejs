0c8ce8fc9ab07f043ce6a28bc7a881cca8fd8a46
Sergio Vago <sergio.vago@hotmail.com>
2020-03-31 09:53:27 -0300

    Curso finalizado

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
-const express = require('express')
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
-const app = express()
+// Iniciando o App
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello SR')
-})
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+  { useNewUrlParser: true, useUnifiedTopology: true });
+requireDir('./src/models');
 
-app.listen(3001)
+
+// Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
